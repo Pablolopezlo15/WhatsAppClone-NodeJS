8b4979d6e2aead78da1e2001b6a19d8a58eb1f7b
Pablolopezlo15 <plopezlozano12@gmail.com>
2024-03-02 17:55:38 +0100

    Update app.js

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/archivosComp', express.static('public/archivosComp'));
 
 // FILE UPLOAD
 app.use(fileUpload()); 
